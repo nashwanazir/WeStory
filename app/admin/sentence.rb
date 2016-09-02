17df2e6eba0ab81c844181b9a03ed83fc573fc58
Nashwa Nazir <nashwanazir149@gmail.com>
2016-09-02 17:00:10 +0000

    add permit params to admin files

--- app/admin/sentence.rb
+++ app/admin/sentence.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Sentence do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :sentence_number, :story_id, :body
 #
 # or
 #
@@ -12,6 +12,9 @@ ActiveAdmin.register Sentence do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+index do
+  column :sentence_number
+  column :body
+end
 
 end
