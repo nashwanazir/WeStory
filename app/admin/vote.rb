d8da774f80b9ea989c5471142988098b017ce602
Nashwa Nazir <nashwanazir149@gmail.com>
2016-09-03 16:22:13 +0000

    WIP: trying carousel on home page

--- app/admin/vote.rb
+++ app/admin/vote.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Vote do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :sentence_id, :user_id
+ permit_params :sentence_id, :user_id  
 #
 # or
 #
