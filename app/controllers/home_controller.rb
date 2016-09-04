3a42b10736d2c3e00bfe6eb7c7da755dd42287b1
Nashwa Nazir <nashwanazir149@gmail.com>
2016-09-04 16:12:54 +0000

    Add stories to carousel slides

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @story= Story.first
-    @sentences= @story.sentences.order(:sentence_number) if @story.present?
+    @story1=Story.first
+    @sentences1= @story1.sentences.order(:sentence_number) if @story1.present?
+    @story2=Story.second
+    @sentences2=@story2.sentences.order(:sentence_number) if @story2.present?
+    @story3=Story.third
+    @sentences3=@story3.sentences.order(:sentence_number) if @story3.present?
   end  
+  
 end
