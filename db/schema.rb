c9c3e0d30bbe502fa00e13a19e7fe26249e9f98f
Nashwa Nazir <nashwanazir149@gmail.com>
2016-09-22 07:58:02 +0000

    clean code

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160830163757) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -28,18 +30,18 @@ ActiveRecord::Schema.define(version: 20160830163757) do
   end
 
   create_table "admin_users", force: :cascade do |t|
-    t.string   "email", default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count", default: 0, null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -48,8 +50,8 @@ ActiveRecord::Schema.define(version: 20160830163757) do
     t.integer  "story_id"
     t.text     "body"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "sentence_number"
   end
 
