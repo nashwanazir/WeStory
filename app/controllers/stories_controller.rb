67694aa6e60b1718cf929e07f957ec73ef12436c
Nashwa Nazir <nashwanazir149@gmail.com>
2016-09-23 14:44:53 +0000

    Add flash message to users once story is created

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,25 +2,26 @@ class StoriesController < ApplicationController
   def index
     @stories = Story.all
   end
-  
+
   def new
     @story = Story.new
     @story.sentences.build
   end
-  
+
   def story_params
     params.require(:story).permit(:title, sentences_attributes: [:id, :body])
   end
-   
+
   def create
     @story = Story.new(story_params)
     if @story.save
-      redirect_to action: 'index'
+      flash[:success] = "Story successfully created"
+      redirect_to @story
     else
       render action: 'new'
     end
   end
-  
+
   def show
     @story = Story.find(params[:id])
   end
