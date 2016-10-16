0ccda36428b2d2fd8afa6d0c57ea7e9075c79259
Nashwa Nazir <nashwanazir149@gmail.com>
2016-10-16 13:30:13 +0000

    Enforce maximum length for sentence

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,6 +1,7 @@
 class Sentence < ApplicationRecord
   belongs_to :story
   validates :sentence_number, uniqueness: { scope: :story_id }
+  validates :body, length: { maximum: 500 }
   belongs_to :user
   has_many :votes
 end
